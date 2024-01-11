72804d300e62d6ee952fb6f726c6a32a1dff1116
HasanMajidi <ha3an.majidii@gmail.com>
2024-01-11 20:42:41 +0330

    Update app.py for register blueprint general

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from blueprints.admin import app as admin
+from blueprints.general import app as general
 
 app = Flask(__name__)
 
 app.register_blueprint(admin)
+app.register_blueprint(general)
 
 
 if __name__ == '__main__':
