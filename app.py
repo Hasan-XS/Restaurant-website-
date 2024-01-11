a4e10b666e00f5c16715650c27f4092f805ae730
HasanMajidi <ha3an.majidii@gmail.com>
2024-01-11 23:30:00 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from blueprints.admin import app as admin
 from blueprints.general import app as general
+import config
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = config.SECRET_KEY
 app.register_blueprint(admin)
 app.register_blueprint(general)
 
