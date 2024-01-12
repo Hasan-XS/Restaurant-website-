2e6fd3a4d8d154e912b58e4a3a16734e91121cd7
HasanMajidi <ha3an.majidii@gmail.com>
2024-01-12 20:47:26 +0330

    Create food model table

---
+++ Model/food.py
@@ -0,0 +1,11 @@
+from sqlalchemy import *
+from extention import db
+
+
+class Food(db.Model):
+    __tablename__ = "Food"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True, nullable=False, index=True)
+    description = Column(String, nullable=False, index=True)
+    active = Column(Integer, nullable=False, index=True)
+    price = Column(Integer, nullable=False, index=True)
