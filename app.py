789259b4e9dcd04193c8ab5bedb0518c99d74c46
HasanMajidi <ha3an.majidii@gmail.com>
2024-01-12 20:38:35 +0330

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ from flask import Flask
 from blueprints.admin import app as admin
 from blueprints.general import app as general
 import config
+import extention
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = config.SECRET_KEY
+app.config["SQLALCHEMY_DATABASE_URI"] = config.SQLALCHEMY_DATABASE_URI
+extention.db.init_app(app)
 app.register_blueprint(admin)
 app.register_blueprint(general)
+
 
 
 if __name__ == '__main__':
