8911273e7915fe19b8290bb45464de414706ff0a
HasanMajidi <ha3an.majidii@gmail.com>
2024-01-14 23:11:56 +0330

    Updated admin panel for save food on database

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -1,6 +1,9 @@
+from turtledemo.chaos import f
+
 from flask import Blueprint, render_template, session, request,abort, redirect
 import config
 from Model.food import Food
+from extention import db
 
 app = Blueprint("admin",__name__)
 
@@ -26,9 +29,28 @@ def login():
         return render_template("admin/login.html")
     
 # admin add food
-@app.route("/admin/food")
-def food():
-    return render_template("admin/food.html")
+@app.route('/admin/food', methods=["GET", "POST"])
+def foods():
+    if request.method == "GET":
+        return render_template("admin/food.html")
+    else:
+        name = request.form.get('name', None)
+        description = request.form.get('description', None)
+        price = request.form.get('price', None)
+        active = request.form.get('active', None)
+        file = request.files.get('file', None)
+
+        f = Food(name=name, description=description, price=price)
+        if active == None:
+            f.active = 0
+        else:
+            f.active = 1
+
+        db.session.add(f)
+        db.session.commit()
+
+        file.save(f"static/food/{f.id}.jpg")
+        return "done"
 
 
 # admin dashboard routes
